b47c31d7e13ae93ec93d657e0f8aedfb88fd653c
Maria Cosmen <mcosme00@gmail.com>
2023-01-07 16:03:18 +0900

    NEW - CREATE - EDIT - UPDATE - DESTROY actions

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -5,9 +5,12 @@ class ArticlesController < ApplicationController
   end
 
   def new
+    @article = Article.new
   end
 
   def create
+    @article = Article.create!(article_params)
+    redirect_to article_path(@article)
   end
 
   def show
@@ -15,13 +18,24 @@ class ArticlesController < ApplicationController
   end
 
   def edit
+    @article = Article.find(params[:id])
   end
 
   def update
-
+    @article = Article.find(params[:id])
+    @article.update(article_params)
+    redirect_to article_path(@article)
   end
 
   def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+    redirect_to articles_path
   end
 
+  private
+
+  def article_params
+    params.require(:article).permit(:title, :content)
+  end
 end
